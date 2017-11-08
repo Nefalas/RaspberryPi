bb4bb84f73fe2febc21c2d97f86211a6362c93bb
Alexis Lindgren <alexisli@kth.se>
2017-11-08 19:27:14 +0100

    Picture test

--- ArduCAM4Pi/streamer.c
+++ ArduCAM4Pi/streamer.c
@@ -113,7 +113,7 @@ int main(int argc, char *argv[]) {
 
   size_t img_len = capture();
 
-  printf("Length: %s\n", img_len);
+  printf("Length: %d\n", img_len);
 
   // Open the new file
   FILE *fp1 = fopen(argv[2], "w+");
